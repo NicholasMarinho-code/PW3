cc26ec3a56c148cecb15876686aa73cedcd75139
NicholasMarinho-code <nearmarinhos@gmail.com>
2026-06-08 11:12:05 -0300

    Atualização do Config.php 2

--- meufrontend/TCC/config.php
+++ meufrontend/TCC/config.php
@@ -1,6 +1,6 @@
 <?php
 $host = "aws-0-us-east-1.pooler.supabase.com";
-$port = "6543";
+$port = "5432";
 $dbname = "postgres";
 $user = "postgres.glvdrydonrsmguxvtuoh";
 $password = "SuperMario@202021";
