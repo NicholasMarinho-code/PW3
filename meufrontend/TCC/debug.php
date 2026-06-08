a5366f2ff5a26d5414c3444eb6632cb989a4b6e5
NicholasMarinho-code <nearmarinhos@gmail.com>
2026-06-08 13:42:48 -0300

    Debug 2

--- meufrontend/TCC/debug.php
+++ meufrontend/TCC/debug.php
@@ -1,34 +1,20 @@
 <?php
 
-$host = "aws-0-us-east-1.pooler.supabase.com";
-$port = "6543";
-$dbname = "postgres";
+try {
 
-$usuarios = [
-    "postgres",
-    "postgres.glvdrydonrsmguxvtuoh"
-];
+    $pdo = new PDO(
+        "pgsql:host=aws-0-us-east-1.pooler.supabase.com;port=6543;dbname=postgres;sslmode=require",
+        "postgres.glvdrydonrsmguxvtuoh",
+        "SUA_SENHA",
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::PGSQL_ATTR_DISABLE_PREPARES => true
+        ]
+    );
 
-$senha = "SUA_SENHA";
+    echo "CONECTOU";
 
-foreach ($usuarios as $user) {
+} catch (PDOException $e) {
 
-    echo "<h2>Testando: $user</h2>";
-
-    try {
-
-        $pdo = new PDO(
-            "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require",
-            $user,
-            $senha
-        );
-
-        echo "CONECTOU";
-
-    } catch (PDOException $e) {
-
-        echo $e->getMessage();
-    }
-
-    echo "<hr>";
+    die($e->getMessage());
 }
