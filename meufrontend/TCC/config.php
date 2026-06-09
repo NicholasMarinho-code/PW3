30d487c3a0409bfe86c0c181115ef53be06530b3
NicholasMarinho-code <nearmarinhos@gmail.com>
2026-06-09 19:14:31 -0300

    Corrigindo docker e nginx

--- meufrontend/TCC/config.php
+++ meufrontend/TCC/config.php
@@ -1,25 +1,14 @@
 <?php
-
 $host = "db.glvdrydonrsmguxvtuoh.supabase.co";
 $port = "5432";
 $dbname = "postgres";
-
 $user = "postgres";
 $password = "SuperMario@202021";
-
 try {
-
-    $pdo = new PDO(
-        "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require",
-        $user,
-        $password
-    );
-
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
+    $pdo = new PDO($dsn, $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    echo "CONECTOU";
-
 } catch (PDOException $e) {
-
-    die($e->getMessage());
+    echo "Erro: " . $e->getMessage();
 }
+?>
